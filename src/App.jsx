bbe2228c9e0da98395567f811d63efe257dc7dc2
denischagin <denischaginnn@gmail.com>
2023-02-18 19:39:06 +0300

    material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,82 @@
 import React from 'react';
 import { useRef, useState } from 'react';
+import { Button, Grid, IconButton, Typography } from '@material-ui/core';
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import Timer from './Timer';
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+	start: {
+		color: theme.palette.success.main,
+		fontSize: "20px"
+	},
+	pause: {
+		color: theme.palette.primary.main
+	},
+	stop: {
+		color: theme.palette.error.main
+	},
+}))
 
 function App() {
-  const [count, setCount] = useState(0);
-  const timerRef = useRef();
-
-  const msToHMSM = (ms) => {
-    ms *= 10;
-    let milliseconds = Math.floor((ms % 1000) / 10);
-    let seconds = Math.floor((ms / 1000) % 60);
-    let minutes = Math.floor((ms / (1000 * 60)) % 60);
-    let hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
-
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-    milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
-
-    return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
-  }
-
-  const startTimer = () => {
-    clearInterval(timerRef.current)
-    timerRef.current = setInterval(() => {
-      setCount(prev => prev + 1)
-    }, 10)
-    setPause(false)
-  }
-
-  const stopTimer = () => {
-    clearInterval(timerRef.current)
-    setCount(0)
-  }
-
-  const pauseTimer = () => {
-    clearInterval(timerRef.current)
-  }
+	const [count, setCount] = useState(0);
+	const timerRef = useRef();
+	const classes = useStyles()
 
 
-  return (
-    <div className="app">
-      <p className='timer__text'>
-        {msToHMSM(count)}
-      </p>
-      <button onClick={startTimer}>▶️ Старт</button>
-      <button onClick={pauseTimer}>⏸ Пауза</button>
-      <button onClick={stopTimer}>⏹ Стоп</button>
-    </div>
-  );
+	const startTimer = () => {
+		clearInterval(timerRef.current)
+		timerRef.current = setInterval(() => {
+			setCount(prev => prev + 1)
+		}, 10)
+	}
+
+	const stopTimer = () => {
+		clearInterval(timerRef.current)
+		setCount(0)
+	}
+
+	const pauseTimer = () => {
+		clearInterval(timerRef.current)
+	}
+
+
+	return (
+		<Grid
+			container
+			direction='column'
+			justifyContent='center'
+			alignItems='center'
+		>
+			<Typography variant='h5'>Ты опоздал на:</Typography>	
+			<Timer ms={count} />
+			<Grid
+				container
+				direction='row'
+				justifyContent='center'
+				alignItems='center'
+				spacing={2}
+			>
+				<Grid item>
+					<IconButton onClick={startTimer} className={classes.start}>
+						<PlayCircleFilledIcon sx={{ fontSize: "80px" }}/>
+					</IconButton>
+				</Grid>
+				<Grid item>
+					<IconButton onClick={pauseTimer} className={classes.pause}>
+						<PauseCircleIcon sx={{ fontSize: "80px" }}/>
+					</IconButton>
+				</Grid>
+				<Grid item>
+					<IconButton onClick={stopTimer} className={classes.stop}>
+						<StopCircleIcon sx={{ fontSize: "80px" }} />
+					</IconButton>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default App;
---
+++ src/Timer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+const Timer = ({ ms }) => {
+    const msToHMSM = (ms) => {
+        ms *= 10;
+        let milliseconds = Math.floor((ms % 1000) / 10);
+        let seconds = Math.floor((ms / 1000) % 60);
+        let minutes = Math.floor((ms / (1000 * 60)) % 60);
+        let hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        milliseconds = (milliseconds < 10) ? "0" + milliseconds : milliseconds;
+
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+    }
+
+    return (
+        <Typography variant='h4' align='center'>{msToHMSM(ms)}</Typography>
+    )
+}
+
+export default Timer
